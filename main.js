7151a752f2331a98aa18104ed34f29dcc3d911e3
kmlissa <kmlissa@aggies.ncat.edu>
2020-09-24 00:42:20 -0400

    added home page and links

---
+++ main.js
@@ -0,0 +1,15 @@
+function eraseBorder(txt){
+    txt.style.borderColor = "white";
+}
+
+function border(txt){
+    txt.style.borderColor = "black";
+}
+
+function clickFacultyDirectory(){
+    window.location.href = 'FacultyDirectory.html';
+}
+
+function clickEvents() {
+    window.location.href = 'Events.html';
+}
